1b7e1ec6c58e135388829aa84076dcbcf2b2ae28
Vladimir Kirillov <kirill-vladimir@yandex.ru>
2022-09-29 01:26:03 +0300

    rev 2.5

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,8 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='default')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(' ')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(' ')
 
 
 # Application definition
